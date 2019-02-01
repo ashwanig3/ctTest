abed5f1f4259a9f72649a45a81c44dd977adfc80
ashwanig3 <goswami17829@gmail.com>
2019-02-01 17:56:59 +0530

    generate multiple receipts

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,31 +5,31 @@ class Form {
     this.form = document.createElement('form');
     this.node.appendChild(this.form);
     this.form.innerHTML = `
-        <input type="number" required name="monthlyRent" placeholder="Monthly Rent">
-        <input type="text" required name="houseAddress"  placeholder="House Address">
-        <input type="text" required name="tenantName" placeholder="Tenant's name">
-        <input type="text" required name="ownerName" placeholder="Owner's name">
-        <input type="text" required name="ownerPAN" placeholder="owner's PAN">
+        <input value="20000" type="number" required name="monthlyRent" placeholder="Monthly Rent">
+        <input value="Rakker Dharamshala" type="text" required name="houseAddress"  placeholder="House Address">
+        <input value="Ashwani Goswami" type="text" required name="tenantName" placeholder="Tenant's name">
+        <input value="Gururaj Singh" type="text" required name="ownerName" placeholder="Owner's name">
+        <input value="CTUPK1324" type="text" required name="ownerPAN" placeholder="owner's PAN">
         <label for="start">Start Date</label>
-        <input type="date" required name="startDate" id="start">
+        <input value="2018-02-01" type="date" required name="startDate" id="start">
         <label for="end">End Date</label>
-        <input type="date" required name="endDate" id="end">
+        <input value="2018-05-01" type="date" required name="endDate" id="end">
         <button>Submit</button>
         `;
     this.form.addEventListener('submit', e => {
       e.preventDefault();
       var userData = Object.values(this.form).reduce((acc, val) => {
-        acc[val.name] = val.value;
+        if (val.name === 'startDate' || val.name === 'endDate') {
+          acc[val.name] = val.valueAsDate;
+        } else {
+          acc[val.name] = val.value;
+        }
         return acc;
       }, {});
+
+      console.log(userData);
       var userInfo = new Receipt(userData);
       this.node.replaceChild(userInfo.node, this.form);
     });
-  }
-  addDaysToMonth() {
-    var startDate = new Date(this.userData.startDate);
-    var addedDate = new Date(startDate);
-
-    addedDate.setDate(addedDate.getDate() + 30);
   }
 }
--- assets/js/reciept.js
+++ assets/js/reciept.js
@@ -2,40 +2,76 @@ class Receipt {
   constructor(data) {
     this.userData = data;
     this.node = document.createElement('div');
+    this.singleReceipt = document.createElement('div');
+    this.singleReceipt.classList.add('single-receipt');
     this.node.classList.add('receipt-container');
+    this.monthsData = [];
+
+    this.multipleMonths = this.addMultipleReciepts(this.userData);
+
     this.monthName = {
-      '01': 'January',
-      '02': 'February',
-      '03': 'March',
-      '04': 'April',
-      '05': 'May',
-      '06': 'June',
-      '07': 'July',
-      '08': 'August',
-      '09': 'September',
-      '10': 'October',
-      '11': 'November',
-      '12': 'December',
+      1: 'January',
+      2: 'February',
+      3: 'March',
+      4: 'April',
+      5: 'May',
+      6: 'June',
+      7: 'July',
+      8: 'August',
+      9: 'September',
+      10: 'October',
+      11: 'November',
+      12: 'December',
     };
-    this.node.innerHTML = `
-            <div class="receipt-header">
-            <h1 class="receipt-heading">Rent Receipt</h1>
-            <span class="date">${
-              this.monthName[this.userData.endDate.split('-')[1]]
-            } - ${this.userData.endDate.split('-')[0]}</span>
+
+    this.node.innerHTML += this.monthsData
+      .map((monthData, i) => {
+        return (this.singleReceipt.innerHTML = `
+        <div class="receipt-item">
+            <div class="receipt-header-wrapper">
+            <div>
+              <div class="receipt-header">
+              <h1 class="receipt-heading">Rent Receipt</h1>
+              <span class="date">${
+                this.monthName[monthData.endDate.getMonth()]
+              } - ${monthData.endDate.getFullYear()}</span>
+              </div>
+              <span class="description">Generated on cleartax(cleartax.in/save/rent)</span>
             </div>
-            <span>Generated on cleartax(cleartax.in/save/rent)</span>
-            <p>Received sum of INR Rs.${this.userData.monthlyRent} from ${
-      this.userData.tenantName
-    } towards the rent of property located at ${
-      this.userData.houseAddress
-    } for the
-            period from ${this.userData.startDate} to ${this.userData.endDate}
-            </p>
-            <div class="regard">
-                <span>${this.userData.ownerName}</span>
-                <span>PAN: ${this.userData.ownerPAN}</span>
+            <div>
+              <span class="side-bar">Receipt No: ${i + 1}</span>
+              <span class="side-bar">Date: ${monthData.endDate.getDate()}-${
+          this.monthName[monthData.endDate.getMonth()]
+        }-${monthData.endDate.getFullYear()} </span>
             </div>
-        `;
+          </div>
+          <p>Received sum of INR Rs.${monthData.monthlyRent} from ${
+          monthData.tenantName
+        } towards the rent of property located at ${
+          monthData.houseAddress
+        } for the
+          period from ${monthData.startDate} to ${monthData.endDate}
+          </p>
+          <div class="regard">
+              <span>${monthData.ownerName}</span>
+              <span>PAN: ${monthData.ownerPAN}</span>
+          </div>
+        </div>
+  `);
+      })
+      .join('');
+  }
+  add30Days(initialDate) {
+    var finalDate = new Date(initialDate);
+    finalDate.setDate(finalDate.getDate() + 30);
+
+    return finalDate;
+  }
+  addMultipleReciepts(userInfo) {
+    var nextMonth = this.add30Days(userInfo.startDate);
+    if (userInfo.startDate < userInfo.endDate) {
+      this.monthsData.push({...userInfo, endDate: nextMonth});
+      this.addMultipleReciepts({...userInfo, startDate: nextMonth});
+    }
   }
 }
